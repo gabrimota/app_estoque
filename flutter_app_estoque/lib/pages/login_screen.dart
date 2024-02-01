dc5769eff7243381b0ef52a5406033a8bbd4dbca
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-02-01 11:36:26 -0300

    código login screen

--- flutter_app_estoque/lib/pages/login_screen.dart
+++ flutter_app_estoque/lib/pages/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+// tela de login do app
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -8,7 +8,31 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // criei uma app bar com um texto "Login Page" para mostrar ao usuário que ele está na pag de login
       appBar: AppBar(title: const Text("Login Page")),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              // Primeiro campo de acesso (E-mail ou Usuário)
+              decoration: InputDecoration(
+                  labelText: "E-mail / Usuário",
+                  hintText: "Digite seu E-mail / Usuário"),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            // Segundo campo de acesso (senha )
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Senha",
+                hintText: "Digite a sua Senha",
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
